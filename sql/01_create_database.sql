4f1e8e2259be016c29ab3051301bb099fa8f5b52
Sejal Priya <priyasejal.13@gmail.com>
2026-07-19 01:03:31 +0530

    Initial project setup and database creation script

---
+++ sql/01_create_database.sql
@@ -0,0 +1,17 @@
+/*
+====================================================
+Project : Smart Banking Management System
+Database: smart_banking_system
+Version : 1.0
+Author  : Sejal Priya
+====================================================
+*/
+
+-- Drop database if it already exists
+DROP DATABASE IF EXISTS smart_banking_system;
+
+-- Create database
+CREATE DATABASE smart_banking_system;
+
+-- Select database
+USE smart_banking_system;
