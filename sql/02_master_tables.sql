6bc50f51b61b42b1874508cde85c46c4102279ff
Sejal Priya <priyasejal.13@gmail.com>
2026-07-20 12:48:43 +0530

    feat(database): create employees table

--- sql/02_master_tables.sql
+++ sql/02_master_tables.sql
@@ -50,3 +50,56 @@ CREATE TABLE Branches
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
         ON UPDATE CURRENT_TIMESTAMP
 );
+/*=========================================================
+ TABLE: Employees
+ Description:
+ Stores employee information.
+=========================================================*/
+
+CREATE TABLE Employees
+(
+    employee_id INT AUTO_INCREMENT PRIMARY KEY,
+
+    employee_code VARCHAR(15) NOT NULL UNIQUE,
+
+    branch_id INT NOT NULL,
+
+    first_name VARCHAR(50) NOT NULL,
+
+    last_name VARCHAR(50) NOT NULL,
+
+    gender ENUM('MALE','FEMALE','OTHER')
+        NOT NULL,
+
+    date_of_birth DATE NOT NULL,
+
+    email VARCHAR(100)
+        NOT NULL UNIQUE,
+
+    phone VARCHAR(15)
+        NOT NULL UNIQUE,
+
+    designation VARCHAR(60)
+        NOT NULL,
+
+    hire_date DATE
+        NOT NULL,
+
+    salary DECIMAL(12,2)
+        NOT NULL,
+
+    employment_status
+        ENUM('ACTIVE','INACTIVE','RESIGNED')
+        DEFAULT 'ACTIVE',
+
+    created_at TIMESTAMP
+        DEFAULT CURRENT_TIMESTAMP,
+
+    updated_at TIMESTAMP
+        DEFAULT CURRENT_TIMESTAMP
+        ON UPDATE CURRENT_TIMESTAMP,
+
+    CONSTRAINT fk_employee_branch
+        FOREIGN KEY(branch_id)
+        REFERENCES Branches(branch_id)
+);
