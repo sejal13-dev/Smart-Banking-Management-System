cd844893d100238b330262ab5806afccbabbe611
Sejal Priya <priyasejal.13@gmail.com>
2026-07-20 12:42:33 +0530

    feat(database): create branches table

---
+++ sql/02_master_tables.sql
@@ -0,0 +1,52 @@
+/*
+=========================================================
+Project : Smart Banking Management System
+File    : 02_master_tables.sql
+Author  : Sejal Priya
+Version : 1.0
+
+Description:
+Creates all master/reference tables required
+for the Smart Banking Management System.
+
+=========================================================
+*/
+
+USE smart_banking_system;
+
+/*=========================================================
+ TABLE: Branches
+ Description:
+ Stores information about all bank branches.
+=========================================================*/
+
+CREATE TABLE Branches
+(
+    branch_id INT AUTO_INCREMENT PRIMARY KEY,
+
+    branch_code VARCHAR(10) NOT NULL UNIQUE,
+
+    branch_name VARCHAR(100) NOT NULL,
+
+    ifsc_code VARCHAR(15) NOT NULL UNIQUE,
+
+    address_line1 VARCHAR(150) NOT NULL,
+    address_line2 VARCHAR(150),
+
+    city VARCHAR(50) NOT NULL,
+    state VARCHAR(50) NOT NULL,
+
+    pincode CHAR(6) NOT NULL,
+
+    phone VARCHAR(15) NOT NULL,
+
+    email VARCHAR(100) UNIQUE,
+
+    branch_status ENUM('ACTIVE','INACTIVE')
+        DEFAULT 'ACTIVE',
+
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        ON UPDATE CURRENT_TIMESTAMP
+);
---
+++ sql/tasksync.sql
@@ -0,0 +1,1 @@
+create database tasksync_db;
